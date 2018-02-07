4949e560054af7a41653c58735d4401538f897ba
kokkein <kokkein@hotmail.com>
2018-02-07 17:23:09 +0800

    Initial commit

---
+++ App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './src/reducers';
+import firebase from 'firebase';
+
+
+class App extends Component {
+  componentWillMount(){
+    const config = {
+      apiKey: "AIzaSyCP9we10UmgO8fvaVv99pID1rwSNwy0rjc",
+      authDomain: "mobileapp-d5c87.firebaseapp.com",
+      databaseURL: "https://mobileapp-d5c87.firebaseio.com",
+      projectId: "mobileapp-d5c87",
+      storageBucket: "mobileapp-d5c87.appspot.com",
+      messagingSenderId: "651300618867"
+    };
+    firebase.initializeApp(config);
+  }
+
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Text>
+            hello!
+          </Text>
+        </View>
+      </Provider>
+    );
+  }
+}
+
+export default App;
